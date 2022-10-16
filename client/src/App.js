b180e6bf2320f30cb840f9d1ff3b92418331dbaa
Vivian Liu <41202975+vzliu@users.noreply.github.com>
2022-10-16 04:28:28 -0700

    make form data go to backend but server cant fulfill request

--- client/src/App.js
+++ client/src/App.js
@@ -23,19 +23,40 @@ function App() {
         ))
       )}*/
 
-  this.uploadFile = async(e) => {
+      /*
+      async function uploadFile(e) {
+      */
+
+  //this.uploadFile = async(e) => {
+    async function uploadFile(e) {
     const message_folder = e.target.files;
     if (message_folder != null) {
       const data = new FormData();
       data.append('message_folder', message_folder);
 
-      let response = await fetch('/upload',
+      /*let response = await fetch('/upload',
       {
         method: 'post',
         body: data,
       });
       let result = await response.json();
       if (result.status !== 1) {
+        alert('Error uploading file')
+      }*/
+
+      const response = await fetch("/upload", 
+      {
+        // Data will be serialized and sent as json
+        method: "POST",
+        body: data,
+            
+        // stuff we want to send
+        // tell the server we're sending JSON
+        headers: {
+            "Content-Type": "application/json"
+        },
+      });
+      if (!response.ok) {
         alert('Error uploading file')
       }
     }
@@ -46,7 +67,7 @@ function App() {
         <h3>Click the button to upload your data</h3> <br />
       </div>
       <label htmlFor="contained-button-file">
-          <input type="file" webkitdirectory="" mozdirectory="" directory="" onChange={this.uploadFile} />
+          <input type="file" webkitdirectory="" mozdirectory="" directory="" onChange={uploadFile} />
       </label>
     </div>
   );
