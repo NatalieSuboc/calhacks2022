b7f36bbc9f59296807bc666a097cd9bbbfc06975
Jessica <jessicaliang26@gmail.com>
2022-10-15 23:58:16 -0700

    text-analyzer.py skeleton

--- flask-server/text-analyzer.py
+++ flask-server/text-analyzer.py
@@ -4,12 +4,16 @@ class TextAnalyzer:
     def __init__(self): 
         self.parser = JsonParser()
 
-    # Example of a simple feature 
+    # Example
     def featureExample(self): 
-        participants = self.parser.get_participants('andreakwong_xqvcy4ubsq')
-        self.parser.get_messages('andreakwong_xqvcy4ubsq')
-        return participants
-        
+        chat_list = self.parser.get_chats_list()
+        chatZeroParticipants = self.parser.get_participants(chat_list[0])
+        chatZeroData = self.parser.chat_data[chat_list[0]]
+        return
+
+    # Add Additional Features Here 
+
+
 if __name__ == "__main__":
     text_analyzer = TextAnalyzer() 
     print(text_analyzer.featureExample())
