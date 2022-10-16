b1290c5eeb1ddd97c068fd33a1e22fd3ae3560b2
Natalie Suboc <nsuboc@berkeley.edu>
2022-10-16 01:59:24 -0700

    Added load autofill JSON info, get_common_words_used

--- flask-server/parser.py
+++ flask-server/parser.py
@@ -8,6 +8,7 @@ class JsonParser:
         self.chats_list = os.listdir(self.dir_path)
         # 'self.chat_data': dict('chat_name' -> 'data')
         self.chat_data = self.load_chat_data()
+        self.autofill_info = self.load_autofill_info()
 
     def load_chat_data(self):
         """
@@ -27,6 +28,17 @@ class JsonParser:
             chat_data[chat_name] = data
             # Fields: messages,title,is_still_participant,thread_type,thread_path,magic_words,joinable_mode,participants
         return chat_data
+
+    def load_autofill_info(self):
+        """
+        Reads in autofill information (metadata)
+        :return:
+        """
+        autofill_info = {}
+        filename = "../messages/autofill_information.json"        
+        f = open(filename)
+        data = json.load(f)
+        return data
 
     def get_chats_list(self):
         """
@@ -52,6 +64,13 @@ class JsonParser:
         """
         return self.chat_data[chat_name]['messages']
         # sender_name, timestamp_ms, content, reactions/actor
+
+    def get_user_name(self):
+        """
+        Returns user name
+        :return:
+        """
+        return self.autofill_info['autofill_information_v2']['FULL_NAME'][0]
 
 
 if __name__ == "__main__":
--- flask-server/text-analyzer.py
+++ flask-server/text-analyzer.py
@@ -1,4 +1,6 @@
 from parser import JsonParser
+import nltk
+from nltk.corpus import stopwords
 
 class TextAnalyzer: 
     def __init__(self): 
@@ -12,8 +14,31 @@ class TextAnalyzer:
         return
 
     # Add Additional Features Here 
-
+    '''
+    Feature: Most Common Words Used
+    - Discerns most common words used by the user.
+    '''
+    def get_common_words_used(self, name, num=20):
+        word_counts = {} # key: word, count: value
+        for chat in self.parser.get_chats_list():
+            messages = self.parser.get_messages(chat)
+            for message in messages:
+                # add words if written by the user 
+                if message['sender_name'] == name and 'content' in message:
+                    text = message['content']
+                    words = text.split(' ')
+                    for word in words:
+                        if word in word_counts.keys():
+                            word_counts[word] += 1
+                        else:
+                            word_counts[word] = 1
+        sorted_counts = sorted(word_counts.items(), key=lambda x: x[1], reverse=True)
+        word_counts = dict(sorted_counts)
+        words = [word for word in list(word_counts) if word.lower() not in set(stopwords.words('english'))]
+        return words[:num]
 
 if __name__ == "__main__":
     text_analyzer = TextAnalyzer() 
     print(text_analyzer.featureExample())
+    print(text_analyzer.get_common_words_used('Natalie Suboc', num=60))
+    # print(text_analyzer.get_common_words_used('Harmony He', num=40))
